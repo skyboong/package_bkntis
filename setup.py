6d2943b6ff878881732de2449147ab72bdca1127
BK Choi <stsboongkee@gmail.com>
2023-06-15 14:19:36 +0900

    initial commit

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name='bkntis',
     version='0.0.1',
     packages=['bkntis'],
-    url='https://github.com/skyboong/bkntis',
-    license='GNU GENERAL PUBLIC LICENSE',
+    url='https://github.com/skyboong/package_bkntis',
+    license='GNU GENERAL PUBLIC LICENSE V3',
     author='B K Choi',
     author_email='stsboongkee@gmail.com',
     description='This is a specialized analysis tool for analyzing national research and development investment amounts in South Korea.'
