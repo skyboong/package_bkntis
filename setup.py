5699c790123a1b1673d4016279ec73c66d8be879
BK Choi <stsboongkee@gmail.com>
2023-06-27 08:25:58 +0900

    수정 3 commit

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
 name = 'bkntis'
-packages = ['bkntis']
+packages = ['bkntis', 'bkntis/util', 'bkntis/graph']
 keywords = ['R&D Investment', "R&D Funding"]
 description = 'Python package for analyzing and visualizing the R&D investment in South Korea.'
-install_requires = ['pandas', 'plotly','matplotlib','XlsxWriter','PyPDF2']
+install_requires = ['pandas', 'plotly','matplotlib','XlsxWriter','PyPDF2', 'scipy', 'kaleido','openpyxl']
 
 if __name__ == "__main__":
     setup(
         name=name,
-        version='0.0.1',
+        version='0.0.2',
         packages=packages,
         url='https://github.com/skyboong/package_bkntis',
         license='GNU GENERAL PUBLIC LICENSE V3',
@@ -17,7 +17,7 @@ if __name__ == "__main__":
         author_email='stsboongkee@gmail.com',
         description=description,
         keywords=keywords,
-        #install_requires=install_requires,
-        #python_requires=">=3.10",
+        install_requires=install_requires,
+        python_requires=">=3.10",
     )
 
